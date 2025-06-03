d71f85750b98229abe344a00c08bdc6f20267434
AnonimMofc <Seluhinvalentin@gmail.com>
2025-06-03 23:50:44 +0300

    itsfortest.py

--- Directtoimages/itsfortest.py
+++ Directtoimages/itsfortest.py
@@ -7,9 +7,9 @@ import pyautogui
 import threading
 
 def play_sound():
-    playsound('negrimolosi.mp3') # Название звуквой дорожки + формат(smth.mp3/smth.wav)   |||  Name of sound + format(smth.mp3/smth.wav)
+    playsound('') # Название звуквой дорожки + формат(smth.mp3/smth.wav)   |||  Name of sound + format(smth.mp3/smth.wav)
 
-img = cv2.imread('badbad.mp3') # Название картинки + формат(smth.jpg и т.д.)   ||| Name of pic + format(smth.jpg etc.)
+img = cv2.imread('') # Название картинки + формат(smth.jpg и т.д.)   ||| Name of pic + format(smth.jpg etc.)
 
 screen_width, screen_height = pyautogui.size()
 resized_img = cv2.resize(img, (screen_width, screen_height), interpolation=cv2.INTER_AREA)
